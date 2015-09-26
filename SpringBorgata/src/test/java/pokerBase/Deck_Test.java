fc1905d15798a4bbebf167af5654afa55bdb244f
Manuel <Manuel@128.4.168.118>
2015-09-26 12:36:17 -0400

    Final Commit and Push

--- SpringBorgata/src/test/java/pokerBase/Deck_Test.java
+++ SpringBorgata/src/test/java/pokerBase/Deck_Test.java
@@ -1,6 +1,6 @@
 package pokerBase;
 
-import static org.junit.Assert.*;
+import org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -10,32 +10,32 @@ import org.junit.Test;
 
 public class Deck_Test {
 	
-	Deck d;
+	public Deck d;
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		
+	public void setUpBeforeClass() throws Exception {
+		d = new Deck(52);
 	}
 
 	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	public void tearDownAfterClass() throws Exception {
+		d = null;
 	}
 
 	@Before
 	public void setUp() throws Exception {
-		d = new Deck(52);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		d = null;
 	}
 
 	@Test
 	public void TestFullDeck() {
 		//	I put this test in so it would pass...
 		//TODO: Code a unit test to make sure calling the deck constructor passes back 52 cards in a deck.
-		TestFullDeck = new Deck(52);
+		assertTrue(d.getTotalCards() == 52);
+		assertFalse(d.getTotalCards() != 52);
 		
 		assertTrue(1==1);
 	}
